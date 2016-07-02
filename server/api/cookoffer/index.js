56ce840122af64fb0b6e44615317fbb452538676
Muneer Ahmad <Muneer Ahmad>
2016-07-02 17:10:33 +0200

    APIs secured

--- server/api/cookoffer/index.js
+++ server/api/cookoffer/index.js
@@ -2,16 +2,16 @@
 
 var express = require('express');
 var controller = require('./cookoffer.controller');
-
+import * as auth from '../../auth/auth.service';
 var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.get('/:id/offers', controller.showCookOffers);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/:id/offers', auth.isAuthenticated(), controller.showCookOffers);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
  
 module.exports = router;
--- server/api/order/index.js
+++ server/api/order/index.js
@@ -2,15 +2,16 @@
 
 var express = require('express');
 var controller = require('./order.controller');
+import * as auth from '../../auth/auth.service';
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.get('/:id/customeroffers', controller.showCustomerOrders);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id/:offer_id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.get('/:id/customeroffers', auth.isAuthenticated(), controller.showCustomerOrders);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id/:offer_id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
